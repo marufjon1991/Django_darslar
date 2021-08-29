0cc8ce08bd459ecf127e4bcc7bf477eb12d780a1
Marufjon Khaliev <88569516+marufjon1991@users.noreply.github.com>
2021-08-30 02:03:41 +0800

    add new files

--- block_project/blog/urls.py
+++ block_project/blog/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import BlogPageView, BlogDetailView, BlogCreateView
+from .views import (
+    BlogPageView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
 
 
 urlpatterns = [
+    path('post/<int:pk>/delete',BlogDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/edit',BlogUpdateView.as_view(),name = 'post_edit'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='detailView'),
     path('', BlogPageView.as_view(), name='home'),
     path('post/new/', BlogCreateView.as_view(), name='new_post')
-
 ]
