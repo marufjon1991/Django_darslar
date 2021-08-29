54eb4769735b00d23c6de096ce0831a36414bd61
Marufjon Khaliev <88569516+marufjon1991@users.noreply.github.com>
2021-08-29 20:57:31 +0800

    add

--- block_project/blog/urls.py
+++ block_project/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BlogPageView, BlogDetailView
+from .views import BlogPageView, BlogDetailView, BlogCreateView
+
 
 urlpatterns = [
+    path('post/new/', BlogCreateView.as_view(), name='new_post'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='detailView'),
     path('', BlogPageView.as_view(), name='home'),
 
--- block_project/blog/views.py
+++ block_project/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 # Create your views here.
@@ -10,3 +11,8 @@ class BlogPageView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'detailView.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title','author','body']
