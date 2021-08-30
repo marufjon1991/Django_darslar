f84c7cc3797df202874b006edc4b4427f893ab2d
Marufjon Khaliev <88569516+marufjon1991@users.noreply.github.com>
2021-08-30 21:03:55 +0800

    bug fixed

--- block_project/accounts/views.py
+++ block_project/accounts/views.py
@@ -6,7 +6,7 @@ from django.views import generic
 
 # Create your views here.
 
-class SignUpView(UserCreationForm):
+class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
