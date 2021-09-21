a4d46fe5eacfc25c2d7bdff1b6189e2fb14108be
Marufjon Khaliev <88569516+marufjon1991@users.noreply.github.com>
2021-09-21 14:38:32 +0800

    add files

--- my_final_project/config/urls.py
+++ my_final_project/config/urls.py
@@ -22,8 +22,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('pages.urls'))
+    path('articles/', include('articles.urls')),
+    path('', include('pages.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
